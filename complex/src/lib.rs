084450df03788f0b7a8ae036fcdb6fbd914180f2
Fabrice <fab.hatege15@gmail.com>
2023-07-20 22:17:02 +0200

    Fixed normal number for complex values

--- complex/src/lib.rs
+++ complex/src/lib.rs
@@ -5,15 +5,36 @@ use nom::combinator::opt;
 use nom::bytes::complete::tag;
 use nom::IResult;
 
-pub fn parse_complex(s: &str) -> IResult<&str, Language> {
+pub fn floating_point(s: &str) -> IResult<&str, String> {
+    let res = tuple((tag("."), digit1))(s);
+    match res {
+        Ok((s, (p, d))) => Ok((s, format!("{}{}", p, d))),
+        Err(e) => Err(e)
+    }
+}
+
+pub fn parse_real(s: &str) -> IResult<&str, String> {
     let res = tuple((
                opt(tag("-")),
                digit1,
+               opt(floating_point)
+                  ))(s);
+    match res {
+        Ok((s, (Some(m), v, Some(r)))) => Ok((s, format!("{}{}{}", m, v, r))),
+        Ok((s, (Some(m), v, None))) => Ok((s, format!("{}{}", m, v))),
+        Ok((s, (None, v, Some(r)))) => Ok((s, format!("{}{}", v, r))),
+        Ok((s, (None, v, None))) => Ok((s, format!("{}", v))),
+        Err(e) => Err(e)
+    }
+}
+
+pub fn parse_complex(s: &str) -> IResult<&str, Language> {
+    let res = tuple((
+               parse_real,
                tag("i")
                   ))(s);
     match res {
-        Ok((s, (Some(m), v, r))) => Ok((s, Language::Value(format!("{}{}{}", m, v, r), Type::Complex))),
-        Ok((s, (None, v, r))) => Ok((s, Language::Value(format!("{}{}", v, r), Type::Complex))),
+        Ok((s, (v, i))) => Ok((s, Language::Value(format!("{}{}", v, i), Type::Complex))),
         Err(e) => Err(e)
     }
 }
@@ -23,7 +44,7 @@ pub fn parse_complex(s: &str) -> IResult<&str, Language> {
 mod tests {
     use base_language::{Language, Type};
     use nom;
-    use super::parse_complex;
+    use super::{parse_complex, parse_real};
 
     #[test]
     fn test_complex_1() {
@@ -37,6 +58,13 @@ mod tests {
         assert_eq!(
             parse_complex("251i").unwrap().1,
             Language::Value("251i".to_string(), Type::Complex));
+    }
+    
+    #[test]
+    fn test_parse_real() {
+        assert_eq!(
+            parse_real("251").unwrap().1,
+            "251".to_string());
     }
     
 }
